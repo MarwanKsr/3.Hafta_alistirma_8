47d79fafa1d885f0a9527640f1a2fc4c22874746
Marwan <Marwan.kaseer1997@gmail.com>
2022-03-09 00:42:05 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace alistirma_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cumleyi giriniz: ");
+            string cumle = Console.ReadLine();
+            for (int i=0; i<cumle.Length; i++)
+            {
+                if (cumle[i] == ' ')
+                {
+                   string yeni = cumle.Substring(0,i);
+                   Console.WriteLine(yeni);
+                   break;
+                }
+            }
+            
+        }
+    }
+}
